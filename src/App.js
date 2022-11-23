76bf23f97641b7e1d687d6465ed743a2cdfb7a08
Hector Romero <hectorromerodev@gmail.com>
2022-11-23 11:37:52 -0800

    update app with theme provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
+import { ThemeProvider } from '@mui/material/styles';
 import { Button, Container } from '@mui/material';
 import { useEffect } from 'react';
 
-import './App.css';
+import theme from './styles/theme';
+
+// COmponents
+import Appbar from './components/Appbar';
 
 function App() {
   useEffect(() => {
     document.title = 'React Material UI - Home';
   }, []);
-
+ 
   return (
-    <Container
-      maxWidth="xl"
-    >
-      <Button>Test</Button>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container
+        maxWidth="xl"
+        sx={{
+          background: '#FFF',
+        }}
+      >
+        <Appbar />
+        {
+          // 1. Add app bar
+          // 2. Add Hero section
+          // 3. Add Promotion banner
+          // 4. Add Title
+          // 5. Add Products
+          // 6. Add Footer
+          // 7. Add Search Box
+          // 8. Add Drawer
+        }
+        <Button>Test</Button>
+      </Container>
+    </ThemeProvider>
   );
 }
 
