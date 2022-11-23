34726a8047704bf39991c3d307eef1db3f42a541
Hector Romero <hectorromerodev@gmail.com>
2022-11-23 11:38:29 -0800

    added appbar component mobile / desktop

---
+++ src/components/Appbar/index.js
@@ -0,0 +1,17 @@
+import { useTheme } from '@mui/material/styles'
+import { useMediaQuery } from '@mui/material'
+import AppbarMobile from './AppbarMobile';
+import AppbarDesktop from './AppbarDesktop';
+
+export default function Appbar() {
+  
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('md'));
+
+  return (
+    <>
+      {matches ? <AppbarMobile/> : <AppbarDesktop/> }
+    </>
+  )
+  
+}
